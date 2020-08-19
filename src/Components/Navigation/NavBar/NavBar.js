91b9e19414867ffcfa39bd220faaafe1e6c5bb08
Nijin-26 <nijinazarkpz@gmail.com>
2020-08-19 15:47:32 +0530

    Navbar Component is added

--- src/Components/Navigation/NavBar/NavBar.js
+++ src/Components/Navigation/NavBar/NavBar.js
@@ -1,10 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+import classes from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <div>
-      <h1> Header </h1>
-    </div>
+    <nav className={classes.navbar}>
+      <Link to="/">
+        <img
+          src="https://i.imgur.com/brElRal.png"
+          className={classes.navbar__logo}
+        />
+      </Link>
+      <div className={classes.searchArea}>
+        <input type="text" className={classes.searchInput} />
+        <SearchIcon className={classes.searchIcon} />
+      </div>
+
+      <div className={classes.navLinks}>
+        <Link to="/login" className={classes.link}>
+          <div className={classes.option}>
+            <span className={classes.optionOne}>Hello Nijin</span>
+            <span className={classes.optionTwo}>Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className={classes.link}>
+          <div className={classes.option}>
+            <span className={classes.optionOne}>Returns</span>
+            <span className={classes.optionTwo}>& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className={classes.link}>
+          <div className={classes.option}>
+            <span className={classes.optionOne}>Your</span>
+            <span className={classes.optionTwo}>Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className={classes.link}>
+          <div className={classes.optionBasket}>
+            <ShoppingBasketIcon />
+            <span className={classes.basketCount}>0</span>
+          </div>
+        </Link>
+      </div>
+    </nav>
   );
 };
 
